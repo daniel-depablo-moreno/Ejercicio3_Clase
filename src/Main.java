a9a0e148da3425da1b64a61692901ed6c71b93a7
Daniel de Pablo <danieldpmatm@gmail.com>
2025-06-22 15:39:01 +0200

    Created calculator's menu

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido a la calculadora de la clase Main");
     }
+    public void menu(){
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+
+    }
 }
