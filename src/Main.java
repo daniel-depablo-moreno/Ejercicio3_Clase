d30492990440406edc4aa3ab5a0e30fe91e82291
Daniel de Pablo <danieldpmatm@gmail.com>
2025-06-22 16:13:17 +0200

    Sarted menu in main file and created operations class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido a la calculadora de la clase Main");
+        menu();
     }
-    public void menu(){
+    public static void menu(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("1. Sumar");
         System.out.println("2. Restar");
         System.out.println("3. Multiplicar");
         System.out.println("4. Dividir");
+        System.out.println("5. Salir");
+        System.out.println("Seleccione una opción: ");
+        int option=0;
+        try{
+            option=Integer.parseInt(scanner.nextLine());
+            switch (option){
+                case 1:
+                    System.out.println("Seleccionó sumar");
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    menu();
+                    break;
+            }
+        } catch(NumberFormatException e){
+            System.out.println("Por favor, ingrese un número válido");
+            menu();
 
+        }
     }
 }
