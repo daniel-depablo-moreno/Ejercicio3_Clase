c4c7879f94941f6db3d6af3c0de5654465506204
Daniel de Pablo <danieldpmatm@gmail.com>
2025-06-23 12:58:35 +0200

    Added multiplication

--- Ejercicio2/src/Main.java
+++ Ejercicio2/src/Main.java
@@ -36,6 +36,18 @@ public class Main {
                     System.out.println("El resultado de la resta es: " + operations.substract(a, b));
                     menu();
                     break;
+                case 3:
+                    System.out.println("Seleccionó multiplicar");
+                    System.out.println("Ingrese el primer número: ");
+                    a = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Ingrese el segundo número: ");
+                    b = Integer.parseInt(scanner.nextLine());
+                    System.out.println("El resultado de la multiplicación es: " + operations.multiply(a, b));
+                    menu();
+                    break;
+                case 5:
+                    System.out.println("Saliendo de la calculadora. ¡Hasta luego!");
+                    break;
                 default:
                     System.out.println("Opción no válida");
                     menu();
